705c4eaf706688a2616cf256a587acd6d4f8a111
dimy20 <enzostrindade16@outlook.com>
2023-05-06 14:06:39 -0300

    some vector math

--- include/vec.h
+++ include/vec.h
@@ -7,6 +7,20 @@ typedef struct{
 	double x, y;
 }Vec2f;
 
+// vec2 operations
+double Vec2f_len(const Vec2f v);
+Vec2f Vec2f_add(Vec2f a, Vec2f b);
+Vec2f Vec2f_sub(Vec2f a, Vec2f b);
+Vec2f Vec2f_mul(Vec2f a, double t);
+double Vec2f_dot(Vec2f a, Vec2f b);
+
+// vec3 operations
+double Vec3f_len(const Vec3f v);
+Vec3f Vec3f_add(Vec3f a, Vec3f b);
+Vec3f Vec3f_sub(Vec3f a, Vec3f b);
+Vec3f Vec3f_mul(Vec3f a, double t);
+Vec3f Vec3f_cross(Vec3f a, Vec3f b);
+double Vec3f_dot(Vec3f a, Vec3f b);
 Vec3f Vec3f_rotate_z(const Vec3f v, double angle);
 Vec3f Vec3f_rotate_y(const Vec3f v, double angle);
 Vec3f Vec3f_rotate_x(const Vec3f v, double angle);
--- src/vec.c
+++ src/vec.c
@@ -1,6 +1,7 @@
 #include "vec.h"
 #include <math.h>
 
+// vec3 operations
 inline Vec3f Vec3f_rotate_z(const Vec3f v, double angle){
 	double rotated_x = v.x * cos(angle) - v.y * sin(angle);
 	double rotated_y = v.x * sin(angle) + v.y * cos(angle);
@@ -20,4 +21,54 @@ inline Vec3f Vec3f_rotate_y(const Vec3f v, double angle){
 	double rotated_z = v.x * sin(angle) + v.z * cos(angle);
 	Vec3f rotated = {.x = rotated_x, .y = v.y, .z = rotated_z};
 	return rotated;
+}
+
+inline Vec3f Vec3f_cross(Vec3f a, Vec3f b){
+	double x, y, z;
+	x = (a.y * b.z) - (b.y * a.z);
+	y = (a.x * b.z) - (b.x * a.z);
+	z = (a.x * b.y) - (b.x * a.y);
+	return (Vec3f){.x = x, .y = y, .z = z};
+};
+
+inline double Vec3f_len(Vec3f v){
+	return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+inline Vec3f Vec3f_add(Vec3f a, Vec3f b){
+	return (Vec3f){.x = a.x + b.x, .y = a.y + b.y, .z = a.z + b.z};
+}
+
+inline Vec3f Vec3f_sub(Vec3f a, Vec3f b){
+	return (Vec3f){.x = a.x - b.x, .y = a.y - b.y, .z = a.z - b.z};
+}
+
+inline Vec3f Vec3f_mul(Vec3f a, double t){
+	return (Vec3f){.x = a.x * t, .y = a.y * t, .z = a.z * t};
+}
+
+inline double vec3f_dot(Vec3f a, Vec3f b){
+	return a.x*b.x + a.y*b.y + a.z*b.z;
+}
+
+// vec2 operations
+
+inline double Vec2f_len(Vec2f v){
+	return sqrt(v.x * v.x + v.y * v.y);
+}
+
+inline Vec2f Vec2f_add(Vec2f a, Vec2f b){
+	return (Vec2f){.x = a.x + b.x, .y = a.y + b.y};
+}
+
+inline Vec2f Vec2f_sub(Vec2f a, Vec2f b){
+	return (Vec2f){.x = a.x - b.x, .y = a.y - b.y};
+}
+
+inline Vec2f Vec2f_mul(Vec2f a, double t){
+	return (Vec2f){.x = a.x * t, .y = a.y * t};
+};
+
+inline double Vec2f_dot(Vec2f a, Vec2f b){
+	return a.x*b.x + a.y*b.y;
 }
